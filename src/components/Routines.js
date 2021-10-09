ed0622cb89916ede7ca3d6a20b458864b6a7f6b5
Jacob Merkel <soccerjacobjm17@gmail.com>
2021-10-09 15:45:10 -0400

    routines.js

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,10 +1,50 @@
 import React, {useState, useEffect } from 'react'
 import { BASE_URL } from './Api'
+import RenderRoutines from './renderRoutines'
 
-export default function Routines() {
+export default function Routines(props) {
+
+    const {AuthenticatedHeader, UnauthenticatedHeader, loggedInUser, setLoggedinUser} = props
+    const [routineList, setRoutineList] = useState([])
+
+    function checkIfLoggedIn() {
+        if(localStorage.getItem('currentUserToken')) {
+            const currentToken = localStorage.getItem('currentUserToken')
+            useEffect(() => {
+                setLoggedinUser(currentToken)
+            }, [])
+        }
+    }
+
+    checkIfLoggedIn()
+
+    async function fetchRoutines() {
+        try {
+            const result = await fetch(`${BASE_URL}/routines`)
+
+            setRoutineList(response.data.routines)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchRoutines()
+    }, [])
+
+
     return (
-        <div>
-            Routines
-        </div>
+        <>
+        {loggedInUser ? AuthenticatedHeader : null}
+        {!loggedInUser ? UnauthenticatedHeader : null}
+
+        {routineList.map((routine) => {
+            return <RenderRoutines
+                name={routine.name}
+                goal={routine.goal}
+                creatorName={routine.creatorName}
+                />
+        })}
+        </>
     )
 }
